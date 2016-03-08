e276b807bd57cfdce06d2c9a3ede81213d5990d7
xlxfoxxlx <joshfox87@gmail.com>
2016-03-08 03:21:02 -0500

    update ids to r17 and bump to 3.7

--- products/aosip_hammerhead.mk
+++ products/aosip_hammerhead.mk
@@ -14,8 +14,8 @@ PRODUCT_MANUFACTURER := lge
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29Q/2480792:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29Q 2480792 release-keys" \
-    BUILD_DISPLAY_ID=MMB29Q \
-    BUILD_ID=MMB29Q
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MMB29V/2554798:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MMB29V 2554798 release-keys" \
+    BUILD_DISPLAY_ID=MMB29V \
+    BUILD_ID=MMB29V
 
