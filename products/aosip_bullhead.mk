679c4d296264dc1a2c8a2e7bc05bd56d2f0546b0
zymphad <zymphad@live.com>
2016-03-27 00:49:17 -0400

    bullhead MHC19J

--- products/aosip_bullhead.mk
+++ products/aosip_bullhead.mk
@@ -15,8 +15,7 @@ PRODUCT_MANUFACTURER := LGE
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MMB29V/2554798:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 6.0.1 MMB29V 2554798 release-keys"
-    BUILD_ID=MMB29V \
-    BUILD_DISPLAY_ID=MMB29V
+    BUILD_FINGERPRINT=google/bullhead/bullhead:6.0.1/MHC19J/2595691:user/release-keys \
+    BUILD_ID=MHC19J \
+    BUILD_DISPLAY_ID=MHC19J
 
